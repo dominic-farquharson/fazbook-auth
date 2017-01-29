70e8a6924ef324e77b7af6b66aa854332c414cbe
Dominic Farquharson <farquharsondominic@gmail.com>
2017-01-29 16:24:30 -0500

    add express-session, passport, new routes

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+/*
+Importing middleware:
+  express, path, logger, cookieParser, bodyParser
+*/
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-// add new modules and files here
+/*
+importing session, passport, authRoutes, userRoutes
+modules.
+
+*/
+const session = require('express-session');
+const passport = require('passport');
+
+// importing index from routes directory
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth.js');
+const userRoutes = require('./routes/user.js');
+
+/*
+invoking express function and setting it to a
+variable
+*/
+const app = express();
+
+// load environment variables
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,10 +42,20 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // add new express-session and passport middleware here
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add route middleware here
+app.use('/', index);
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 
 // catch 404 and forward to error handler
